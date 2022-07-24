27e3ed4d9ecfaa587349a6604cc551f871ea0b51
ChristopherMorales <christopher.morales@innovaresol.com>
2022-07-24 15:42:42 -0400

    initialise project & install vector icon

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import colors from './src/theme/colors';
+import fonts from './src/theme/fonts';
+import AntDesign from  'react-native-vector-icons/AntDesign';
+const App = () => {
+  return (
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{color: colors.primary, fontSize: fonts.size.md}}>Hello World <AntDesign name= "stepforward" /></Text>
+
+    </View>
+  )
+}
+
+export default App;
---
+++ src/theme/fonts.ts
@@ -0,0 +1,15 @@
+const size = {
+    xs: 10,
+    sm: 12,
+    default: 14,
+    md: 16,
+};
+
+const weight = {
+    full: '900',
+    bold: 'bold',
+    normal: 'normal',
+    thin: '400'
+}
+
+export default {size, weight};
